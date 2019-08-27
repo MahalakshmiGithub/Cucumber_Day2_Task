2f6309b84daeddd66b0f3c1dce2206e7b33cb307
MahalakshmiGithub <mahachandran2295@gmail.com>
2019-08-27 22:44:50 +0530

    Third day task

--- BulkOfUsersData.java
+++ BulkOfUsersData.java
@@ -1,5 +1,8 @@
 package fis.del;
 
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -26,15 +29,49 @@ public class BulkOfUsersData {
 
 	}
 
-	@When("The users should enter the valid details{string},{string},{string},{string},{string}")
-	public void the_users_should_enter_the_valid_details(String fname, String lname, String email, String address,
-			String phone) {
+	/*
+	 * @When(
+	 * "The users should enter the valid details{string},{string},{string},{string},{string}"
+	 * ) public void the_users_should_enter_the_valid_details(String fname,
+	 * String lname, String email, String address, String phone) {
+	 * driver.findElement(By.xpath("//label[text()='Done']")).click();
+	 * driver.findElement(By.id("fname")).sendKeys(fname);
+	 * driver.findElement(By.id("lname")).sendKeys(lname);
+	 * driver.findElement(By.id("email")).sendKeys(email);
+	 * driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys(address
+	 * ); driver.findElement(By.id("telephoneno")).sendKeys(phone);
+	 * driver.findElement(By.xpath("//input[@name='submit']")).click();
+	 * 
+	 * }
+	 */
+
+	/*
+	 * @When("The users should enter the valid details") public void
+	 * the_users_should_enter_the_valid_details(io.cucumber.datatable.DataTable
+	 * dataTable) { System.out.println("When-1"); List<String> onedlist =
+	 * dataTable.asList(String.class); System.out.println(onedlist);
+	 * driver.findElement(By.xpath("//label[text()='Done']")).click();
+	 * driver.findElement(By.id("fname")).sendKeys(onedlist.get(0));
+	 * driver.findElement(By.id("lname")).sendKeys(onedlist.get(1));
+	 * driver.findElement(By.id("email")).sendKeys(onedlist.get(2));
+	 * driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys(
+	 * onedlist.get(3));
+	 * driver.findElement(By.id("telephoneno")).sendKeys(onedlist.get(4));
+	 * driver.findElement(By.xpath("//input[@name='submit']")).click();
+	 * 
+	 * }
+	 */
+
+	@When("The users should enter the valid details")
+	public void the_users_should_enter_the_valid_details(io.cucumber.datatable.DataTable cust) {
+		Map<String, String> onedmap = cust.asMap(String.class, String.class);
+		System.out.println(onedmap);
 		driver.findElement(By.xpath("//label[text()='Done']")).click();
-		driver.findElement(By.id("fname")).sendKeys(fname);
-		driver.findElement(By.id("lname")).sendKeys(lname);
-		driver.findElement(By.id("email")).sendKeys(email);
-		driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys(address);
-		driver.findElement(By.id("telephoneno")).sendKeys(phone);
+		driver.findElement(By.id("fname")).sendKeys(onedmap.get("fname"));
+		driver.findElement(By.id("lname")).sendKeys(onedmap.get("lname"));
+		driver.findElement(By.id("email")).sendKeys(onedmap.get("email"));
+		driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys(onedmap.get("address"));
+		driver.findElement(By.id("telephoneno")).sendKeys(onedmap.get("phoneno"));
 		driver.findElement(By.xpath("//input[@name='submit']")).click();
 
 	}
